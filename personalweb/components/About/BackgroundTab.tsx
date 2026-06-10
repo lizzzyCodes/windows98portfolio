16d490aa21c052893961cdc851e959837681891f
lizzzyCodes <147952557+lizzzyCodes@users.noreply.github.com>
2026-06-10 14:35:58 -0700

    only updating the background tab about me section

--- personalweb/components/About/BackgroundTab.tsx
+++ personalweb/components/About/BackgroundTab.tsx
@@ -4,15 +4,14 @@ export const BackgroundTab = () => {
   return (
     <>
       <div className={`field-border ${styles.bodyText}`} style={{ padding: "8px" }}>
-        This work is personal to me. My own university journey started with
+        My university journey started with
         advisors discouraging me from majoring in computer science, telling me
         the coursework would be too difficult. That experience shaped a deep
         commitment to uplifting others who face similar barriers especially
         those navigating capacity-constrained majors where the odds can feel
         stacked against you. I want to help build belonging and resilience in
         communities that need it most. When I'm not coding or mentoring, you can
-        find me running, weightlifting, hunting for new coffee shops, or
-        training for my next run.
+        find me training for my next run, weightlifting, or hunting for new coffee shops.
       </div >
     </>
   );
